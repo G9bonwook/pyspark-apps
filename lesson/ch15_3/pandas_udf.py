852c094986843ab9a3754cab4ad17ec9a0a2eed0
G9bonwook <gubon0830@gmail.com>
2026-01-18 13:47:29 +0900

    ⭐️ Chapter 15-3 pandas_udf 코드 작성

--- lesson/ch15_3/pandas_udf.py
+++ lesson/ch15_3/pandas_udf.py
@@ -1,11 +1,12 @@
 from common.ch13_4.base_stream_app import BaseStreamApp
-from pyspark.sql.functions import udf, expr, floor
+from pyspark.sql.functions import pandas_udf, expr, floor
 from pyspark.sql.types import IntegerType
 from datetime import datetime
+import pandas as pd
 import time
 
 
-class BadUdf(BaseStreamApp):
+class PandasUdf(BaseStreamApp):
     def __init__(self, app_name):
         super().__init__(app_name)
         self.app_name = app_name
@@ -15,7 +16,9 @@ class BadUdf(BaseStreamApp):
 
     def main(self):
         self.logger.write_log('info','Job begin')
-        spark = self.get_session_builder().getOrCreate()
+        spark = self.get_session_builder() \
+            .config('spark.sql.execution.arrow.pyspark.enabled', 'true') \
+            .getOrCreate()
         birth_df = spark.range(10000000).toDF("id")  # 임의의 ID 컬럼 생성
         birth_df = birth_df \
             .withColumn(
@@ -23,38 +26,22 @@ class BadUdf(BaseStreamApp):
                 floor(10 + (60 - 10 + 1) * expr('rand()'))
             ).withColumn(
                 'BIRTH_YEAR_DECORATOR',
-                BadUdf.get_birth(' ')
+                PandasUdf.get_birth('age')
             )
-
-        '''
-        아래처럼 udf_get_birth 라는 함수를 생성하여 적용하는 것도 가능
-        udf_get_birth = udf(BadUdf.get_birth2, IntegerType())
-        .withColumn(
-                'BIRTH_YEAR_STT_METHOD',
-                udf_get_birth('age')
-            ))
-        '''
         birth_df.groupBy('BIRTH_YEAR_DECORATOR').count().show(100)
 
         self.logger.write_log('info','Job end')
         time.sleep(600)
 
-    # Decorator 적용 방식
-    @staticmethod
-    @udf(returnType=IntegerType())
-    def get_birth(value):
-        cur_year = datetime.now().year
-        birth_year = cur_year - value
-        return birth_year
 
-    # 함수 적용 방식
     @staticmethod
-    def get_birth2(value):
+    @pandas_udf(returnType=IntegerType())
+    def get_birth(value: pd.Series) -> pd.Series:
         cur_year = datetime.now().year
         birth_year = cur_year - value
         return birth_year
 
 
 if __name__ == '__main__':
-    bad_udf = BadUdf(app_name='bad_udf')
-    bad_udf.main()
+    pandas_udf = PandasUdf(app_name='pandas_udf')
+    pandas_udf.main()
